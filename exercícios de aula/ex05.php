c9e8533bb9e817b70c0b382eda6c91476e85cc79
Jenifer Angelo <jenmangelo@gmail.com>
2022-08-23 11:32:06 -0300

    :)

---
+++ exercícios de aula/ex05.php
@@ -0,0 +1,20 @@
+<?php
+    $nomes = array("jose", "neusa", "antonio", "erika", "tiago", "chico");
+    echo "<br>";
+    echo $nomes[0];
+    echo "<br>";
+    echo $nomes[2];
+    echo "<br>";
+    echo $nomes[4];
+    echo "<br>";
+    echo "<br>";
+    for ($x = 0; $x <= 6; $x++){
+        echo "<br>";
+        echo $nomes[$x];
+    }
+
+    echo "<br>";
+    echo "<br>";
+
+    
+?>
