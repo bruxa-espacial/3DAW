6b2074923f3501a509bdc081ddce827276f2ccd6
Jenifer Angelo <jenmangelo@gmail.com>
2022-08-23 13:51:35 -0300

    primeira tent

--- exercícios de aula/ex05.php
+++ exercícios de aula/ex05.php
@@ -2,9 +2,32 @@
     $nomes = array("jose", "neusa", "antonio", "erika", "tiago", "chico");
     $notas = array(7.5, 7.0, 7.1, 8.0, 8.5, 7.7);
     
+    echo "<form>";
+            echo "<tr>";
+                echo "<th>";
+                echo "nome";
+                echo "</th>";
+                echo "<th>";
+                echo "nota";
+                echo "</th>";
+            echo "</tr>";
     for ($x = 0; $x <= 6; $x++){
-        echo "<br>";
-        echo $nomes[$x];
+            echo "<tr>";
+                echo "<td>";
+                echo "</td>";
+            echo "</tr>";
+        
     }
+    echo "<form>";
 // EXERCÍCIO: imprimir nomes e notas numa tabela
 ?>
+<html>
+    <form action="">
+        <tr>
+            <th></th>
+        </tr>
+        <tr>
+            <th></th>
+        </tr>
+    </form>
+</html>
