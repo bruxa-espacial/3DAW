41bfc37cc9b9e78d77eccb402c133d94ab38be0b
Jenifer Angelo <40490759+bruxa-espacial@users.noreply.github.com>
2022-08-30 12:09:22 -0300

    AAAAAAA

--- exercícios de aula/ex07.php
+++ exercícios de aula/ex07.php
@@ -1,31 +1,15 @@
-<?php
-    $nomes = array("jose", "neusa", "antonio", "erika", "tiago", "chico");
-    $notas = array(7.5, 7.0, 7.1, 8.0, 8.5, 7.7);
-    $x = 0;
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <style>
-        table{
-            border: 2px solid black;
-        }
-
-        td{
-            border:2px red outset;
-            padding: 2px;
-
-        }
-        th{
-            background-color: red;
-            color: black;
-        }
-    </style>
-    <title>If/Else</title>
+    <title>UGH</title>
 </head>
 <body>
+    <?php
+        $nomes = array("jose", "neusa", "antonio", "erika", "tiago", "chico");
+        $notas = array(7.5, 2.0, 7.1, 8.0, 8.5, 5.7);
+        $x = 0;
+    ?>
     <table>
         <tr>
             <th>NOME</th>
@@ -38,28 +22,29 @@
                         echo $nomes[$x];
                     echo "</td>";
                     if ($notas[$x] >= 8){
-        ?>
+                    ?>
                     <td style="background-color: green;">
                         <?php echo $notas[$x]; ?>
                     </td>
-                <?php
+                    <?php
                     } else{
-                        if ($notas[$x] >= 6){(
-                ?>
-                    <td style="background-color: yellow;">
-                        <?php echo $notas[$x]; ?>
-                    </td>
-                <?php
-                    }else{
-                ?>
-                    <td style="background-color: red;">
-                        <?php echo $notas[$x]; ?>
-                    </td>
-                <?php
+                        if ($notas[$x] >= 6){
+                            ?>
+                                <td style="background-color: yellow;">
+                                    <?php echo $notas[$x]; ?>
+                                </td>
+                            <?php
+                        } else{
+                            ?>
+                                <td style="background-color: red;">
+                                    <?php echo $notas[$x]; ?>
+                                </td>
+                            <?php
+                        }
                     }
-                ?>
+                    ?>
+                
         <?php
-            echo "</tr>";
             }
         ?>
     </table>
